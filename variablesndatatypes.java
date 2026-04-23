8e71adac11bdd6af25319eec4b54f2247742b4fd
abhijit-dev12 <rabha3222@gmail.com>
2026-04-23 10:46:56 +0530

    1st file

---
+++ variablesndatatypes.java
@@ -0,0 +1,19 @@
+public class variablesndatatypes {
+    public static void main(String[] args){
+        int age = 22;
+        double price = 199.99;
+        char grade = 'A';
+        boolean isSkibidiFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("price = " + price);
+        System.out.println("Grade: " +grade);
+        System.out.println("Is Skibidi Fun ? " + isSkibidiFun);
+
+        //or else instead of writing the variable name we can also write the value of the variable directly in the print statement like this
+        //like instead of writing the code from line 8 to 11 i can do that in 1 line of code
+
+        System.out.println("Age: " + age + "\nPrice: " + price + "\nGrade:"  + grade + "\nIs Skibidi Fun ?: " + isSkibidiFun);
+
+    }
+}
